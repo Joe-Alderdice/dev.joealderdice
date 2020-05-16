aa913cd29ba15fed17138f6b9fa51e3bf1377756
Joe Alderdice <jgalderdice@outlook.com>
2020-05-16 16:44:11 +0100

    Initial website

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('[data-toggle="twitter"]').tooltip();
+    $('[data-toggle="discord"]').tooltip();
+    $('[data-toggle="github"]').tooltip();
+    $('[data-toggle="gitlab"]').tooltip();
+    $('[data-toggle="twitch"]').tooltip();
+    $('[data-toggle="mail"]').tooltip();
+    $('[data-toggle="instagram"]').tooltip();
+    $('[data-toggle="pierre"]').tooltip();
+});
